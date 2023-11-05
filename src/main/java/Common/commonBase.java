c5e05042409020254251b3a0db723a662bd775c5
HOANG_T_SINH\sinhh <sinhhoang2866@gmail.com>
2023-11-05 18:27:31 +0700

    thêm phần TC đầu tiên

--- src/main/java/Common/commonBase.java
+++ src/main/java/Common/commonBase.java
@@ -1,4 +1,22 @@
 package Common;
 
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class commonBase {
+    public WebDriver driver;
+    public WebDriver initChromeDriver(String URL)
+    {
+        ChromeOptions options = new ChromeOptions();
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+        // For MAC: System.getProperty("user.dir") + "/driver/chromedriver");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(URL);
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        return driver;
+    }
 }
--- src/main/java/Testsuite/Login.java
+++ src/main/java/Testsuite/Login.java
@@ -1,4 +1,14 @@
 package Testsuite;
+import Common.commonBase;
+import org.testng.annotations.*;
 
-public class Login {
+public class Login extends commonBase{
+    @BeforeTest
+    public void OpenChrome(){
+        driver= initChromeDriver("https://www.youtube.com/");
+    }
+    @Test
+    public void Test(){
+        System.out.printf("Đã mở thành công URL");
+    }
 }
